57aa42cb2b13af59ae96d61ba55a18a09c9028b6
singh-shreya321 <shreya,singh@iiitb.org>
2018-12-01 02:06:58 +0530

    Data added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!([
-  {email: "shreyasingh@gmail.com", encrypted_password: "$2a$11$btKWfmKhGz8d6S.Vnlx.3OtROjL/F4mwjDYPjq/8v3C0tyXip.5v2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "nimishagarg@gmail.com", encrypted_password: "$2a$11$nGn6FK4dQSWF55yhHY9VU.l.0H2NbRq1Pe95qXkhTSLRchnKXZQwq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "akshi025@gmail.com", encrypted_password: "$2a$11$920ttnkqAZG7x5epTHoiR.lKYC1LHrTPHSETtCbelh0s4H.AUZOQS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+  {email: "shreyasingh@gmail.com", password: "shreya", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
+  {email: "nimishagarg@gmail.com", password: "nimisha", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
+  {email: "akshi025@gmail.com", password: "akshi025", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
 ])
 Answer.create!([
   {ansno: nil, body: "Sir already mailed.", doubt_id: 2, user_id: 2},
